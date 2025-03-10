3264a713143a9447f51a74b6ad9124ce6574f3b9
Markus Røyset <maroider@protonmail.com>
2025-03-10 14:45:02 +0100

    Finish initial implementation of "sign up" functionality

--- rentals/routes/sign_up.py
+++ rentals/routes/sign_up.py
@@ -1,23 +1,28 @@
-from bottle import route
+import mysql.connector
+
+from argon2 import PasswordHasher
+from bottle import get, post, request
+
 
 from ..components import html, with_navbar
+from .. import db
 
 
-@route("/sign-up")
+@get("/sign-up")
 def sign_up():
     return html(
         "Sign up",
         with_navbar("""
             <main id="sign-up">
                 <div>
-                    <form name="sign-up" id="sign-up" action="sign-up">
+                    <form name="sign-up" id="sign-up" action="" method="post">
                         <h1>Sign up</h1>
-                            <label for="email">Email:</label>
-                            <input type="email" name="email" id="email" placeholder="ola.nordmann@gmail.com">
-                            <label for="password">Password:</label>
-                            <input type="password" name="password" id="password" placeholder="********">
-                            <label for="confirm-password">Confirm password:</label>
-                            <input type="password" name="confirm-password" id="confirm-password" placeholder="********">
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" id="email" placeholder="ola.nordmann@gmail.com" required>
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="password" placeholder="********" required>
+                        <label for="confirm-password">Confirm password:</label>
+                        <input type="password" name="confirm-password" id="confirm-password" placeholder="********" required>
                         <button>Sign up</button>
                         <p>Already have an account?</p>
                         <p><a href="login">Log in instead</a></p>
@@ -26,3 +31,40 @@ def sign_up():
             </main>
         """),
     )
+
+
+@post("/sign-up")
+def sign_up_submit():
+    email = request.forms["email"]
+    password = request.forms["password"]
+    confirm_password = request.forms["confirm-password"]
+
+    if password != confirm_password:
+        # TODO: Better error handling
+        return html("Passwords do not match")
+    if "@" not in email or email[-1] == "@" or email[0] in ["@", "+"]:
+        # More complex sanity checks are likely not worth it.
+        # TODO: Better error handling
+        return html("Error", "Invalid email")
+
+    ph = PasswordHasher()
+    pwhash = ph.hash(password)
+
+    cnx = db.db_cnx()
+
+    cur = cnx.cursor()
+    try:
+        cur.execute(
+            """INSERT INTO UserAccount(Email, PasswordHash)
+               VALUES (%s, %s)""",
+            (email, pwhash),
+        )
+    except mysql.connector.errors.IntegrityError:
+        # TODO: Better error handling
+        return html("Error", "Email is already in use")
+    cnx.commit()
+
+    cur.close()
+
+    # TODO: Send actual confirmation email.
+    return html("Account registered", "Account registered")
