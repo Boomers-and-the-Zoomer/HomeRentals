5e6e07e68f053ec27905ac28d94b7dfe1ec8df90
Markus Røyset <maroider@protonmail.com>
2025-03-10 13:37:10 +0100

    Add dbg() utility function
    This function passes through its only argument and prints out the
    expression that creates it and the resulting value.

--- rentals/util.py
+++ rentals/util.py
@@ -13,3 +13,22 @@ def dict_to_attr_string(dict: dict) -> str:
             string += " "
         string += f'{escape(str(name))}="{escape(str(value))}"'
     return string
+
+
+def dbg(value):
+    import inspect
+    from pprint import pformat
+    from pathlib import Path
+
+    parent_frame = inspect.currentframe().f_back
+    frame_info = inspect.getframeinfo(parent_frame)
+    pos = frame_info.positions
+    line_number = parent_frame.f_lineno
+    file_name = Path(parent_frame.f_code.co_filename).name
+    expression = frame_info.code_context[0][pos.col_offset + 4 : pos.end_col_offset - 1]
+    prefix = f"[{file_name}:{line_number}:{pos.col_offset}] "
+    p = pformat(value, width=80 - len(prefix))
+    p = f"\n[{ '=' * (len(prefix)-3)}] ".join(p.split("\n"))
+    print(f"{prefix}{expression} = {p}")
+
+    return value
