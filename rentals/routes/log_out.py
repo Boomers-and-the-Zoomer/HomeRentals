b47ce2bf5583419aa2cf1475f1f88ca76aac028d
Markus Røyset <maroider@protonmail.com>
2025-03-20 22:16:25 +0100

    Added proper deletion of the session token upon logout

--- rentals/routes/log_out.py
+++ rentals/routes/log_out.py
@@ -1,9 +1,27 @@
 from bottle import post, response
 
+from ..auth import get_session_token
+from .. import db
+
 
 @post("/log-out")
 def log_out():
+    session_token = get_session_token()
+
+    cnx = db.db_cnx()
+    cur = cnx.cursor()
+
+    cur.execute(
+        """
+        DELETE FROM Session
+        WHERE Token=_binary %s
+        """,
+        (session_token,),
+    )
+    cnx.commit()
+
+    cur.close()
+
     response.delete_cookie("Session")
     response.status = 303
     response.add_header("Location", "/")
-    # FIXME: Actually delete the session from the db
