24b77d564f83f0b8e9615bae47b4045945d0e9dd
Eirik <eirikringen@gmail.com>
2025-03-20 18:19:05 +0100

    Added functionality and communication with the database to the user-profile and edit page

--- rentals/routes/user_profile.py
+++ rentals/routes/user_profile.py
@@ -1,13 +1,39 @@
 from bottle import route
-
+from ..auth import requires_user_session, get_session_token
 from ..components import html, with_navbar
+from .. import db
 
 
 @route("/user-profile")
+@requires_user_session()
 def user_profile():
+    cnx = db.db_cnx()
+    cur = cnx.cursor()
+
+    session_token = get_session_token()
+
+    cur.execute(
+        """
+        SELECT FirstName,Lives,Languages,Age,FunFact
+        FROM User, Session
+        WHERE User.Email=Session.Email
+            AND Session.Token=_binary %s
+        """,
+        (session_token,),
+    )
+    (first_name, lives, languages, age, fun_fact) = cur.fetchone()
+    if lives == None:
+        lives = ""
+    if languages == None:
+        languages = ""
+    if age == None:
+        age = ""
+    if fun_fact == None:
+        fun_fact = ""
     return html(
         "User Profile",
-        with_navbar("""
+        with_navbar(
+            f"""
             <main id="user-profile">
                 <div class="top_page">
                     <a href="http://localhost:8080/user-profile/edit" class="button">Edit profile</a>
@@ -21,7 +47,7 @@ def user_profile():
                             <p><u>Host since XXXX</u></p>
                         </div>
                         <div class="name">
-                            <p><b>Morty</b><p>
+                            <p><b>{first_name}</b><p>
                             <p>Host<p>
                         </div>
                     </div>
@@ -29,19 +55,20 @@ def user_profile():
                         <div class="fieldset">
                             <fieldset>
                                 <legend><b>About:</b></legend>
-                                <p><b>Lives in:</b> Toronto, Canada</p>
+                                <p><b>Lives in:</b> {lives}</p>
                                 <br><br>
-                                <p><b>Languages:</b> Norwegian, English, Urdu</p>
+                                <p><b>Languages:</b> {languages}</p>
                                 <br><br>
-                                <p><b>Age:</b> Old</p>
+                                <p><b>Age:</b> {age}</p>
 
                             </fieldset>
                         </div>
                         <div class="fieldset">
                             <fieldset>
                                 <legend><b>Fun fact:</b></legend>
-                                <p>Knows Rick</p>
+                                <p> {fun_fact}</p>
                             </fieldset>
+
                         </div>
                     </div>
                 </div>
@@ -51,5 +78,6 @@ def user_profile():
                     <img src="https://a0.muscache.com/im/pictures/074f8383-2194-4a3d-8f3d-ca8dce154cdb.jpg?im_w=720" class="ad_picture" alt="Property listing">
                 </div>
             </main>
-        """),
+       """,
+        ),
     )
--- rentals/routes/user_profile_edit.py
+++ rentals/routes/user_profile_edit.py
@@ -1,47 +1,111 @@
-from bottle import route
+from bottle import get, post, request, response
 
+from ..auth import requires_user_session, get_session_token
 from ..components import html, with_navbar
+from .. import db
 
 
-@route("/user-profile/edit")
+@get("/user-profile/edit")
+@requires_user_session()
 def user_profile_edit():
+    cnx = db.db_cnx()
+    cur = cnx.cursor()
+
+    session_token = get_session_token()
+
+    cur.execute(
+        """
+        SELECT FirstName,Lives,Languages,Age,FunFact
+        FROM User, Session
+        WHERE User.Email=Session.Email
+            AND Session.Token=_binary %s
+        """,
+        (session_token,),
+    )
+    (first_name, lives, languages, age, fun_fact) = cur.fetchone()
+    if lives == None:
+        lives = ""
+    if languages == None:
+        languages = ""
+    if age == None:
+        age = ""
+    if fun_fact == None:
+        fun_fact = ""
+
+    cur.close()
+
     return html(
         "User Profile edit",
-        with_navbar("""
+        with_navbar(
+            f"""
             <main id="user-profile">
                 <div class="user_profile_edit">
                     <a href="http://localhost:8080/user-profile" class="button_cancel">Cancel</a>
-                    <a href="http://localhost:8080/user-profile" class="button_confirm">Confirm</a>
+                    <button class="button_confirm" type="submit" form="update_info">Confirm</button>
                         <div class="profile_pic">
                             <div class="circle_edit">
                                 <img src="https://cdn.europosters.eu/image/750/83398.jpg" class="profile_picture" alt="Profile picture">
                             </div>
                             <a href="http://localhost:8080/user-profile" class="button_picture">Update picture</a>
                             <p class="item"><b>Name:</b><p>
-                             <textarea>Morty</textarea>
+                             <textarea>{first_name}</textarea>
                         </div>
                     <div class="info">
                         <div class="fieldset">
-                            <form>
+                            <form id="update_info" action="" method="POST">
                                 <fieldset>
                                     <legend><b>About:</b></legend>
-                                    <p><b>Lives in:</b></p>
-                                    <textarea>Toronto, Canada</textarea>
+                                    <label for="lives_form"><b>Lives in:</b></label>
+                                    <textarea id="lives_form" name="lives_form" rows="3" cols="40"style="resize: none;">{lives}</textarea>
                                     <br><br>
-                                    <p><b>Languages:</b></p>
-                                    <textarea>Norwegian, English, Urdu</textarea>
+                                    <label for="languages_form"><b>Languages:</b></label>
+                                    <textarea id="languages_form" name="languages_form" rows="3" cols="40"style="resize: none;">{languages}</textarea>
                                     <br><br>
-                                    <p><b>Age:</b></p>
-                                    <textarea>Old</textarea>         
+                                    <label for="age_form"><b>Age:</b></label>
+                                    <textarea id="age_form" name="age_form" rows="3" cols="40"style="resize: none;">{age}</textarea>         
                                 </fieldset>
                                 <fieldset>
                                     <legend><b>Fun fact:</b></legend>
-                                    <textarea>Knows Rick</textarea>
+                                    <label for="fun_fact_form"></label>
+                                    <textarea id="fun_fact_form" name="fun_fact_form" rows="3" cols="40"style="resize: none;">{fun_fact}</textarea>
                                 </fieldset>
                             </form>
                         </div>
                     </div>
                 </div>
             </main>
-        """),
+        """
+        ),
     )
+
+
+@post("/user-profile/edit")
+@requires_user_session()
+def user_profile_edit():
+    cnx = db.db_cnx()
+    cur = cnx.cursor()
+
+    session_token = get_session_token()
+
+    lives = request.forms["lives_form"]
+    languages = request.forms["languages_form"]
+    age = request.forms["age_form"]
+    fun_fact = request.forms["fun_fact_form"]
+
+    cur.execute(
+        """
+        UPDATE User
+        SET Lives = %s, Languages = %s, Age = %s, FunFact = %s
+        WHERE User.Email=(
+            SELECT Email
+            FROM Session
+            WHERE Session.Token=_binary %s
+            )
+        """,
+        (lives, languages, age, fun_fact, session_token),
+    )
+    cnx.commit()
+    cur.close()
+
+    response.status = 303
+    response.add_header("Location", "/user-profile")
