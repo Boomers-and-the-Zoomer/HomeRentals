13ab740c68bff6417340a69b2bd1fefd22012b79
Markus Røyset <maroider@protonmail.com>
2025-04-07 17:26:54 +0200

    Add check for accidental DB closure

--- rentals/db.py
+++ rentals/db.py
@@ -18,6 +18,14 @@ def db_cnx_init(no_db: bool = False):
 
     my_cnx = mysql.connector.connect(**config)
 
+    def oops_you_closed_the_db():
+        raise Exception(
+            "You have accidentally closed the database. Don't do that, please and thank you :)"
+        )
+
+    my_cnx._super_secret_lose = my_cnx.close
+    my_cnx.close = oops_you_closed_the_db
+
     def _db_cnx():
         print("_db_cnx")
         return my_cnx
@@ -27,7 +35,7 @@ def db_cnx_init(no_db: bool = False):
         global cnx_close
         cnx = db_cnx_init
         cnx_close = lambda: None
-        my_cnx.close()
+        my_cnx._super_secret_lose()
 
     cnx = _db_cnx
     cnx_close = _db_cnx_close
